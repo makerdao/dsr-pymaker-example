7aae46c68eb446da8363e12a63d3555e6c1181cb
Kenton Prescott <kentonprescott@gmail.com>
2020-02-06 09:50:43 -0600

    git submodule add lib/pymaker, rm -rf ./pymaker

--- test.sh
+++ test.sh
@@ -4,16 +4,16 @@
 docker pull makerdao/testchain-pymaker:unit-testing
 
 # Start the docker image and wait for parity to initialize
-pushd ./pymaker
+pushd ./lib/pymaker
 docker-compose up -d
 sleep 2
 popd
 
-PYTHONPATH=$PYTHONPATH:./pymaker py.test -s --cov=dsrdemo --cov-report=term --cov-append test/test_dsrdemo.py $@
+PYTHONPATH=$PYTHONPATH:./lib/pymaker py.test -s --cov=dsrdemo --cov-report=term --cov-append test/test_dsrdemo.py $@
 TEST_RESULT=$?
 
 echo Stopping container
-pushd ./pymaker
+pushd ./lib/pymaker
 docker-compose down
 popd
 
